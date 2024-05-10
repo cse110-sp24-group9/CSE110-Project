240284c16d3ecbe7ec345079176acb5e75eee346
Andrew Pegg <andrewcomputsci2019@gmail.com>
2024-05-10 14:08:42 -0700

    added test jsfile

---
+++ source/place_holder.js
@@ -0,0 +1,8 @@
+/**
+ * @author @andrewcomputsci2019
+ * @name hello function
+ * @summary a function that does nothing and is only for testing purposes of JSDOC
+ */
+function hello(){
+    console.log("hello world");
+}
