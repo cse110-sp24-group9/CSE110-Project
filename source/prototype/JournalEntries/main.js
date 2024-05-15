76ee1645dd9f2030df6befc50425cc10b54fd191
Andrew Lara <130115948+drewlar@users.noreply.github.com>
2024-05-14 21:05:22 -0700

    Major Editting
    I apologize but I have been on a plane doing all these edits and did not push periodically.
    
    I fixed most sizing issues. For the most part, website will scale to show more or less info if user zooms website.

--- source/prototype/JournalEntries/main.js
+++ source/prototype/JournalEntries/main.js
@@ -1,56 +1,43 @@
 const journalEntryList = [
-    "<article class='journal-entry'>\
-        <div id='top'>\
-            <div id='favorite'>★</div>\
-            <div id='title' style='font-weight:bold;'>Task Completed</div>\
-            <div id='time'>10:50pm</div>\
-        </div>\
-        <div id='info'>Completed dev journal prototype</div>\
-        <div id='bottom'>\
-            <div id='tags'>\
-                <div id='label-box'>\
-                <div id='label'>Task</div>\
+    '<article class="journal-entry">\
+            <div id="left-info">\
+              <div id="favorite">★</div>\
             </div>\
-            </div>\
-            <div id='emotion'>😄</div>\
-        </div>\
-    </article>",
-    "<article class='journal-entry'>\
-        <div id='top'>\
-            <div id='favorite'>★</div>\
-            <div id='title' style='font-weight:bold;'>My True Title</div>\
-            <div id='time'>10:25pm</div>\
-        </div>\
-        <div id='info'>Lorem ipsum dolor sit amet, consectetur adipiscing ...</div>\
-        <div id='bottom'>\
-            <div id='tags'>\
-                <div id='label-box'>\
-                    <div id='label' style='background-color: rgb(215,58,74); color:white'>Work</div>\
+            <div id="middle-info">\
+              <div id="title" style="font-weight:bold;">Task Completed</div>\
+              <div id="info">Completed dev journal prototype</div>\
+              <div id="tags">\
+                <div id="label-box">\
+                  <div id="label">Task</div>\
                 </div>\
+              </div>\
             </div>\
-            <div id='emotion'>😕</div>\
-        </div>\
-    </article>",
-    "<article class='journal-entry' >\
-        <div id='top'>\
-            <div id='favorite'></div>\
-            <div id='title' style='font-weight:bold;'>Design Team Meeting</div>\
-            <div id='time'>8:00pm</div>\
-        </div>\
-        <div id='info'>Today I discussed with my design team information about ...</div>\
-        <div id='bottom'>\
-            <div id='tags'>\
-                <div id='label-box'>\
-                    <div id='label'>CSE110</div>\
-                </div>\
-                <div id='label-box'>\
-                    <div id='label'>Draft 2</div>\
-                </div>\
+            <div id="right-info">\
+              <div id="time">10:50pm</div>\
+              <div id="emotion">😄</div>\
             </div>\
-            <div id='emotion'>🙂</div>\
-        </div>\
-    </article>"
-]
+          </article>',
+    '<article class="journal-entry">\
+            <div id="left-info">\
+              <div id="favorite">★</div>\
+            </div>\
+            <div id="middle-info">\
+              <div id="title" style="font-weight:bold;">Design Team Meeting</div>\
+              <div id="info">Today I discussed with my design team information about</div>\
+              <div id="tags">\
+                <div id="label-box">\
+                  <div id="label">CSE110</div>\
+                </div>\
+                <div id="label-box">\
+                  <div id="label">Draft2</div>\
+                </div>\
+              </div>\
+            </div>\
+            <div id="right-info">\
+              <div id="time">10:50pm</div>\
+              <div id="emotion">😄</div>\
+            </div>\
+          </article>']
 
 let entriesNum = 0;
 
