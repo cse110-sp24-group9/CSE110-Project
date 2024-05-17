dcbaad09ca05f95e341875842db2c92aeae93949
Jason Boenjamin <jasonboe510@gmail.com>
2024-05-16 23:18:24 -0700

    Please review! Finished making tasklists web component, Jason & Henry, Fixed a few styling things and ensured the strikethrough works properly on the web component. Not documented before but Brendon created the outline for the template and brough over basic functionality from previous code, Jason and Henry finished it from there

--- source/components/task-list-component.js
+++ source/components/task-list-component.js
@@ -1,4 +1,8 @@
-     //
+     /*
+     Outline of template made by Brendon He
+     Documentation done by Henry Tiet and Jason Boenjamin
+     */
+     
      export default class TaskListItem extends HTMLElement{
     /**
      * @type {ShadowRoot}
@@ -22,6 +26,11 @@
      */
     #addTaskButton
 
+    /**
+     * Documented by Henry Tiet
+     * Constructs the shadow tree with fixed nodes of add task 
+     * and the list
+     */
     constructor(){
         super();
         console.log("custom component");
@@ -31,7 +40,11 @@
          */
 
         const tmpl =  document.getElementById('task-list_template');
-        this.#shadow.appendChild(tmpl.content);
+        /**
+         * Documented by Henry
+         * Appends the template to shadow
+         */
+        this.#shadow.appendChild(tmpl.content.cloneNode(true));
         /**
          * @type {HTMLButtonElement}
          */
@@ -45,26 +58,59 @@
         this.#setupButtons();
     }
 
-
+    /*
+     * Documented by Jason
+     * Converted the old Javascript into a template
+     * Pulled old code and incorporated shadow dom into a template
+    */ 
     #setupButtons(){
+        /**
+         * Documented and added by Jason
+         * initializes the add task button from previous js file
+         */
         this.#addTaskButton.addEventListener('click', () => {
             var newListElement = document.createElement('article');
             newListElement.className = 'task-entry';
             newListElement.innerHTML = `
                 <div class="checkbox">
-                    <input type="checkbox">
+                    <input type="checkbox" class="task-checkbox">
                     <div class="checkmark"></div>
                 </div>
                 <input type="text" class="task-text">
                 <button class="minusbtn">-</button>
             `;
             this.#task_list.appendChild(newListElement);
-          
+            
+            /**
+             * Documented by Jason
+             * Initializes the minus button to remove tasks
+             */
             const minusbtn = newListElement.querySelector('.minusbtn')
             minusbtn.addEventListener('click', function() {
-            newListElement.remove();
-        });
-        });
+                newListElement.remove();
+            });
+
+            /* Author : Henry Tiet
+                Adjusting class calls
+            */
+            // const checkbox = newListElement.querySelector('input[type=checkbox]');
+            const checkbox = newListElement.querySelector('.task-checkbox');
+            const textEntry = newListElement.querySelector('.task-text');
+            
+            /**
+             * Documented by Jason
+             * adds an event listener to do the strikethrough through each tasked checked
+             * switched the parameter from 'input' to 'change' and used the .classlist to add
+             */
+            checkbox.addEventListener('change', function() {
+                if(checkbox.checked) {
+                  textEntry.classList.add('strikethrough');
+                } else {
+                  textEntry.classList.remove('strikethrough');
+                }
+            });
+          });
+        
     }
     
 }
