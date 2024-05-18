1365947253c8a4025ec3c3a6b2daf5347ef91691
Andrew Pegg <54915639+andrewcomputsci2019@users.noreply.github.com>
2024-05-17 21:31:53 -0700

    Delete source/place_holder.js

--- source/place_holder.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author @andrewcomputsci2019
- * @name hello function
- * @summary a function that does nothing and is only for testing purposes of JSDOC, extra here
- */
-function hello(){
-    console.log("hello world");
-}
