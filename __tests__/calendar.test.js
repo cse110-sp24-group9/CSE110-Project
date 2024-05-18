f55602efd730fa40fe982ccafb9bf6cf509a43bc
Lance Tran <thelancetran@gmail.com>
2024-05-17 22:40:15 -0700

    Configured express for testing
    Co-authored-by: gir-ma <gir-ma@users.noreply.github.com>
    Co-authored-by: Andrew Pegg <andrewcomputsci2019@users.noreply.github.com>

--- __tests__/calendar.test.js
+++ __tests__/calendar.test.js
@@ -1,6 +1,6 @@
 describe('Basic user flow for Calendar', () => {
     beforeAll(async () => {
-        await page.goto('http://127.0.0.1:5500/source/pages/Calendar.html');
+        await page.goto('http://localhost:3000/pages/Calendar.html');
     });
     
     it('Testing', async () => {
--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -2,5 +2,11 @@ module.exports = {
     launch: {
         headless: false,
         slowMo: 20
-    }
+    },
+    server: {
+        command: "node test.cjs",
+        port: 3000,
+        launchTimeout: 100000,
+        debug: true,
+      },
 }
