0119fc59f1283484aa4120cdd37f84743a88f916
Lance Tran <thelancetran@gmail.com>
2024-05-19 23:08:16 -0700

    created calendar test branch, included test cases

--- __tests__/calendar.test.js
+++ __tests__/calendar.test.js
@@ -3,6 +3,21 @@ describe('Basic user flow for Calendar', () => {
         await page.goto('http://localhost:3000/pages/Calendar.html');
     });
     
+    // ***** TEST CASES *****
+    // calendar month goes forward when clicking right arrow
+    // calendar month goes backward when click left arrow
+    // year increases by 1 after December
+    // year decreases by 1 before January
+    // current day is highlighted
+    // hovering over a day highlights it
+
+    // correct number of active days
+    // first day in correct day of week position
+    // last day in correct day of week position
+    // should load the calendar
+    // should handle navigation boundary /// changing between months at last day
+    // styling
+
     it('Testing', async () => {
         console.log('Testing...');
         expect(true).toBe(true);
