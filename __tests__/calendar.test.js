e5616f12453bdef62bf75d3a3c3b98ce786f7ee0
Andrew Pegg <andrewcomputsci2019@gmail.com>
2024-05-21 14:55:05 -0700

    created initial test, but snagged on change that needs to be made onto actual calendar component

--- __tests__/calendar.test.js
+++ __tests__/calendar.test.js
@@ -3,8 +3,20 @@ describe('Basic user flow for Calendar', () => {
         await page.goto('http://localhost:3000/pages/Calendar.html');
     });
     
-    it('Testing', async () => {
-        console.log('Testing...');
+    it('Route Testing', async () => {
+        console.log('Testing static page route');
         expect(true).toBe(true);
     });
+
+    it('test that calendar component loads', async () => {
+        /* 
+            need to look into method to load the selector once the calendar has actually
+            hydrated onto the page, right now its not waiting for the days to load
+        */
+        const calendar = await page.waitForSelector('calendar-component');
+        const shadow =  await calendar.getProperty('shadowRoot');
+        let chilldrenNumber = await shadow.$$('span');
+        console.log(chilldrenNumber.length);
+        expect(chilldrenNumber.length > 0).toBe(true);
+    },10000)
 });
