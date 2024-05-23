d424d84f91c4943c294dea70b6626e2a88c0f9a3
Andrew Pegg <andrewcomputsci2019@gmail.com>
2024-05-22 18:25:42 -0700

    Co-authored-by: Lance Tran <thelancetran@users.noreply.github.com> Co-authored-by: gir-ma <gir-ma@users.noreply.github.com>
    initial work on component

---
+++ source/components/JournalCard.js
@@ -0,0 +1,35 @@
+/**
+ * @module JournalCardModule
+ */
+
+/**
+ * @class
+ * @extends {HTMLElement}
+ * @alias journalCard-component
+ * @author Andrew Pegg, Lance Tran, Girma Terfa
+ * @version 1.0.0
+ */
+export default class JournalCard extends HTMLElement {
+    /**
+     * @constructor
+     */
+    constructor(){
+        
+    };
+
+    /**
+     * 
+     */
+    #initClickListener(){
+
+    }
+
+    /*
+        Needs to have ability to favorite and save those changes to the database
+        Needs ability to send out needs-edit event to main page, so modal can open to edit this component
+        Needs ability to have right click context menu with associated actions
+    */
+
+    
+
+}
---
+++ source/components/JournalEntries.js
@@ -0,0 +1,52 @@
+/**
+ * @module JournalEntriesModule
+ */
+
+/**
+ * @class
+ * @extends {HTMLElement}
+ * @alias journalEntries-component
+ * @author Andrew Pegg, Lance Tran, Girma Terfa
+ * @version 1.0.0
+ */
+export default class JournalEntries extends HTMLElement {
+    /**
+     * @constructor
+     */
+    constructor(){
+        
+    };
+    /**
+     * 
+     */
+    addEntry(){
+        // TODO: finish this
+    }
+    /**
+     * This function is responsible for subscribing to the input field, to filter journal entries by finding what entries have the same/contains the search for string
+     * @private
+     * @property {Function} initSearchHandler
+     * @returns {void}
+     * @summary creates the filter handler for the journal component
+     */
+    #initSearchHandler(){
+
+    }
+    /**
+     * 
+     */
+    #initCreateEntry(){
+
+    }
+    /**
+     * This function filters journal entries by subscribing to journal entries tag check boxes
+     * @private
+     * @property {Function} initFilterHandler
+     * @returns {void}
+     * @summary filters entries based on tags selected
+     */
+    #initFilterHandler(){
+
+    }
+    
+};
