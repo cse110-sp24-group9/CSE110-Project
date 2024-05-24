4c8530f5caf5afbfa17af0cd99433753d07a674c
Char1iee <charlieshang1@gmail.com>
2024-05-24 13:22:16 -0700

    Update add label button not finished

--- source/components/Canvas.js
+++ source/components/Canvas.js
@@ -1,1 +1,19 @@
 var simplemde = new SimpleMDE({ element: document.getElementById("markdown-editor") });
+
+document.getElementById('label-add').addEventListener('click', function() {
+    let labelBox = document.createElement('div');
+    labelBox.className = 'label-box';
+    labelBox.innerHTML = `
+      <input type="checkbox">
+      <div class="label" contenteditable="true">New Label</div>
+    `;
+    labelBox.querySelector('input').addEventListener('change', function() {
+      let label = this.nextElementSibling;
+      if (this.checked) {
+        label.style.backgroundColor = '#d3d3d3';
+      } else {
+        label.style.backgroundColor = '';
+      }
+    });
+    document.getElementById('label-bar').insertBefore(labelBox, document.getElementById('label-box-add'));
+  });
