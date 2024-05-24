83bb4049fa6e2437d60100ec2b5e267f89b0cd5f
Andrew Lara <130115948+drewlar@users.noreply.github.com>
2024-05-24 12:40:22 -0700

    Added Ability to close sidebar

---
+++ source/components/Toolbar.js
@@ -0,0 +1,22 @@
+const SideBar = document.querySelector('aside');
+const SidebarButtonOpen = document.querySelector('#sidebar-button-open');
+const SidebarButtonClose = document.querySelector('#sidebar-button-close');
+const ConditionalButtons = document.querySelector('#conditional-buttons')
+
+
+/**
+ * Makes the sidebar pop open
+ * Removes Sidebar and Settings button from the journal entries
+ */
+SidebarButtonOpen.addEventListener('click', () => {
+    SideBar.style.display = 'flex'
+    ConditionalButtons.style.display = 'none'
+});
+/**
+ * Makes the sidebar pop close
+ * Adds Sidebar and Settings button to the journal entries
+ */
+SidebarButtonClose.addEventListener('click', () => {
+    SideBar.style.display = 'none'
+    ConditionalButtons.style.display = 'inline'
+});
