b6a614c95e6cb34e0197d90ea209ae2bb4a1594d
Andrew Lara <130115948+drewlar@users.noreply.github.com>
2024-05-25 20:05:43 -0700

    Added State changing for toolbar
    Menu icons and mode information now changes based on what state a user is in
    - View mode
    - Edit mode

--- source/components/Canvas.js
+++ source/components/Canvas.js
@@ -3,12 +3,39 @@ var simplemde = new SimpleMDE({
      toolbar: ["bold", "italic", "strikethrough", "heading",
                 "|","unordered-list", "ordered-list", "quote", "code",
                 "|", "link", "image", "table",
-                "|", "side-by-side", "fullscreen", "guide"] });
+                "|", "side-by-side", "fullscreen", "guide"]
+});
+
 simplemde.togglePreview();
 const toggleModeButton = document.querySelector("#toggle-mode-button > button");
+const editIcons = document.getElementsByClassName("edit-mode")
+const viewIcons = document.getElementsByClassName("view-mode")
+const modeDisplay = document.querySelector("#mode-info > p")
 
+function hideElements(elementContainer){
+  for(let i = 0; i < elementContainer.length; i++){
+    elementContainer[i].style.display = 'none'
+  }
+}
 
-toggleModeButton.addEventListener('click', () => {simplemde.togglePreview();});
+function showElements(elementContainer, display){
+  for(let i = 0; i < elementContainer.length; i++){
+        elementContainer[i].style.display = display
+    }
+}
+
+toggleModeButton.addEventListener('click', () => {
+  if(simplemde.isPreviewActive()){
+    hideElements(viewIcons)
+    showElements(editIcons, 'inline')
+    modeDisplay.innerHTML = "Edit Mode"
+  }else{
+    hideElements(editIcons)
+    showElements(viewIcons, 'inline')
+    modeDisplay.innerHTML = "View Mode"
+  }
+  simplemde.togglePreview();
+});
 
 document.getElementById('label-add').addEventListener('click', function() {
     let labelBox = document.createElement('div');
