6d65c20df066ea2b7121d023ab4f7fbb4e802190
Andrew Pegg <andrewcomputsci2019@gmail.com>
2024-06-07 20:19:24 -0700

    update

--- source/components/Task-list.js
+++ source/components/Task-list.js
@@ -277,11 +277,6 @@
          * @returns list of tasks
          */
         save(){
-            // let ret_list = [];
-            // for(let i = 0; i < this.tasks.length; i++){
-            //     ret_list.push(structuredClone(this.tasks[i][0]));
-            // }
-            // return ret_list;
             return this.tasks.map((entry) => structuredClone(entry[0]));
         }
 }
